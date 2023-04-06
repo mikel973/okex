2d575e07d8b53517a8e58d214aaf899d5e4eb643
mikel <mikel0lib7@gmail.com>
2023-04-06 16:43:55 +0800

    upate sub-command tickers

--- cmd/okex/market.go
+++ cmd/okex/market.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/mikel973/okex"
 	"github.com/mikel973/okex/logs"
 	requests "github.com/mikel973/okex/requests/rest/market"
 	"github.com/urfave/cli/v2"
@@ -11,6 +12,7 @@ var marketCmd = &cli.Command{
 	Usage: "Market data from okex",
 	Subcommands: []*cli.Command{
 		subTickerCmd,
+		subTickersCmd,
 	},
 }
 
@@ -41,5 +43,41 @@ func getTicker(c *cli.Context) error {
 		logs.GetLog().Infof("Ticker: %+v", tck)
 	}
 
+	return nil
+}
+
+var subTickersCmd = &cli.Command{
+	Name:  "tickers",
+	Usage: "Retrieve the latest price snapshot.",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "inst-type",
+			Usage: "Instrument type, e.g. SPOT/SWAP/FUTURES/OPTION",
+			Value: "SPOT",
+		},
+		&cli.StringFlag{
+			Name:  "uly",
+			Usage: "Underlying, e.g. BTC-USD",
+			Value: "",
+		},
+	},
+	Action: getTickers,
+}
+
+func getTickers(c *cli.Context) error {
+	client := getClient()
+
+	instType := c.String("inst-type")
+	uly := c.String("uly")
+	req := requests.GetTickers{InstType: test.InstrumentType(instType), Uly: uly}
+	resp, err := client.Rest.Market.GetTickers(req)
+	if err != nil {
+		logs.GetLog().Fatal(err)
+	}
+	logs.GetLog().Debugf("Response:%+v", resp)
+	for _, tck := range resp.Tickers {
+		logs.GetLog().Infof("Ticker: %+v", tck)
+	}
+	logs.GetLog().Debug("Inst Type is:", instType, " Uly is:", uly, " Count is:", len(resp.Tickers))
 	return nil
 }
